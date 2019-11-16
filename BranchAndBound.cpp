6e05851440f3697f9e2445b4fc41458cbeec56d9
TulioNPL <tulionp.lopes@gmail.com>
2019-11-16 20:42:44 -0300

    Versao sem bound

--- BranchAndBound.cpp
+++ BranchAndBound.cpp
@@ -2,125 +2,88 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
-#include <limits> 
+#include <climits> 
 #include <stdio.h>
 #include <string.h>
-#include <bits/stdc++.h> 
+
 using namespace std; 
 
 #define INF INT_MAX
 
 int N; 
-int final_path[101]; 
+int caminho_final[101]; //+1 porque precisa voltar ao inicial
 bool visited[100]; 
-int final_res = INF; 
+int menorDist = INF; 
 typedef pair<int,int> ponto;
 int matriz[100][100];   //matriz de adjacencia das cidades(com distancias)
-int cidades[100];   //max 100 cidades
 
 int dist2pontos(ponto a, ponto b) {
     return sqrt(pow((b.first - a.first),2) + pow((b.second - a.second),2));
 }
 
-//Encontra a menor aresta adjacente a uma cidade
-int firstMin(int adj[][100], int i) { 
-    int min = INF; 
-    for (int k=0; k<N; k++) 
-        if (adj[i][k]<min && i != k) 
-            min = adj[i][k]; 
-    return min; 
-} 
+void branchAndBound(int parcial, int level, int caminho_parcial[]) { 
 
-//encontra a segunda menor aresta adjacente a uma cidade
-int secondMin(int adj[][100], int i) { 
-    int first = INF, second = INF; 
-    for (int j=0; j<N; j++) { 
-        if (i == j) 
-            continue; 
-  
-        if (adj[i][j] <= first) { 
-            second = first; 
-            first = adj[i][j]; 
-        } 
-        else if (adj[i][j] <= second && adj[i][j] != first) 
-            second = adj[i][j]; 
-    } 
-    return second; 
-} 
-
-void TSPRec(int adj[][100], int curr_bound, int curr_weight, 
-            int level, int curr_path[]) { 
-
+    //Se chegar na ultima posicao e a distancia for diferente de 0
     if (level==N) { 
-        if (adj[curr_path[level-1]][curr_path[0]] != 0) { 
-            int curr_res = curr_weight + adj[curr_path[level-1]][curr_path[0]]; 
-   
-            if (curr_res < final_res) { 
+        if (matriz[caminho_parcial[level-1]][caminho_parcial[0]] != 0) { 
+            int curr_res = parcial + matriz[caminho_parcial[level-1]][caminho_parcial[0]]; 
+
+            //se a resposta parcial for menor que a final
+            if (curr_res < menorDist) { 
+                //atualiza o melhor caminho
 				for (int i=0; i<N; i++) 
-        			final_path[i] = curr_path[i]; 
-    			final_path[N] = curr_path[0]; 
-                final_res = curr_res; 
+        			caminho_final[i] = caminho_parcial[i]; 
+    			caminho_final[N] = caminho_parcial[0]; 
+                menorDist = curr_res; //atualiza a melhor resposta
             } 
         } 
     } else {
+        //itera pelas cidades do problema
 		for (int i=0; i<N; i++) { 
+            //Se a cidade nao foi visitada e se a distancia eh diferente de 0
+			if (matriz[caminho_parcial[level-1]][i] != 0 && visited[i] == false) { 
+				parcial += matriz[caminho_parcial[level-1]][i]; //atualiza a parcial
 	
-			if (adj[curr_path[level-1]][i] != 0 && visited[i] == false) { 
-				int temp = curr_bound; 
-				curr_weight += adj[curr_path[level-1]][i]; 
-	
-				if (level==1) 
-				curr_bound -= ((firstMin(adj, curr_path[level-1]) + firstMin(adj, i))/2); 
-				else
-				curr_bound -= ((secondMin(adj, curr_path[level-1]) + firstMin(adj, i))/2); 
-	
-				if (curr_bound + curr_weight < final_res) { 
-					curr_path[level] = i; 
+                //Se a parcial for maior que a melhor resposta ja obtida
+                //Ignora-se esse caminho e seus derivados
+				if (parcial < menorDist) { 
+					caminho_parcial[level] = i; 
 					visited[i] = true; 
 
-					TSPRec(adj, curr_bound, curr_weight, level+1, curr_path); 
+					branchAndBound(parcial, level+1, caminho_parcial); 
 				} 
 
-				curr_weight -= adj[curr_path[level-1]][i]; 
-				curr_bound = temp; 
+				parcial -= matriz[caminho_parcial[level-1]][i]; //desfaz a atualizacao da parcial
 	
 				memset(visited, false, sizeof(visited)); 
 				for (int j=0; j<=level-1; j++) 
-					visited[curr_path[j]] = true; 
+					visited[caminho_parcial[j]] = true; 
 			} 
 		} 
 	}
 } 
   
-void TSP(int adj[][100]) { 
-    int curr_path[N+1];  
-    int curr_bound = 0; 
-    memset(curr_path, -1, sizeof(curr_path)); 
-    memset(visited, 0, sizeof(curr_path)); 
-  
-    for (int i=0; i<N; i++) 
-        curr_bound += (firstMin(adj, i) + secondMin(adj, i)); 
-  
-    curr_bound = (curr_bound&1)? curr_bound/2 + 1 : curr_bound/2; 
 
+void TSP() { 
+    int caminho_parcial[N+1];  
+    memset(caminho_parcial, -1, sizeof(caminho_parcial)); 
+    memset(visited, 0, sizeof(caminho_parcial)); 
+ 
     visited[0] = true; 
-    curr_path[0] = 0; 
+    caminho_parcial[0] = 0; 
 
-    TSPRec(adj, curr_bound, 0, 1, curr_path); 
+    branchAndBound(0, 1, caminho_parcial); 
 } 
 
 int main() {
-    double menorDist = std::numeric_limits<double>::infinity();
     cin >> N;
     vector<ponto> coordenadas;
-
 
     //Le as coordenadas de cada cidade e adiciona no vector de coordenadas
     int x, y;
     for(int i = 0; i < N; i++) {
         cin >> x >> y;
         coordenadas.push_back(make_pair(x,y));
-        cidades[i] = i;  //seta o vetor de cidades
     }
 
     //Calcula as distancias entre cada cidade e adiciona na matriz de adjacencia
@@ -132,11 +95,12 @@ int main() {
         }
     }
 
-    TSP(matriz);
+    TSP();
 
-	printf("%d\n", final_res);  
+	printf("%d\n", menorDist);  
     for (int i=0; i<N; i++) 
-        printf("%d ", final_path[i]+1);
+        printf("%d ", caminho_final[i]+1);
 	cout << endl;
+
     return 0;
 }
